8b074fd8e908c17cadedc099522c39c2bd362ee5
ajc-li <allan.jai.li@gmail.com>
2016-06-27 14:11:07 -0600

    Add files via upload
    finished c4 with working diagonals

--- c4goodrender/board.rb
+++ c4goodrender/board.rb
@@ -1,108 +1,165 @@
-class Board 
+class Array
+  def safe_transpose
+    max_size = self.map(&:size).max
+    self.dup.map{|r| r << nil while r.size < max_size; r}.transpose
+  end
+end
+
+
+
+class Board
    def initialize
-       @board = Array.new(6){Array.new(7)}
+       @board = Array.new(7){Array.new(6, "z")}
        @counter = 0
        @column=0
        @rowcoordinate=0
    end
-   
-   def render
-       puts 
-       # if @counter>0
-       #     monkey_around
-       #     counter += 1 
-       # end
 
-       @board.each do |row|
+   def render
+       puts "--------- CONNECT 4 ---------"
+       puts "-----------------------------"
+       @board.safe_transpose.each do |row|
            row.each do |cell|
-               cell.nil? ? print("-") : print(cell.to_s)
+               print "|"
+               if cell == "z"
+                 print("---")
+               else
+                 print(" #{cell.to_s} ")
+               end
            end
+       print "|"
        puts
+       puts "-----------------------------"
        end
-       puts
-       # monkey_around
+
    end
-   # def monkey_around
-   #     @board=[[@board[0][0],@board[1][0],@board[2][0],@board[3][0],@board[4][0],@board[5][0]],
-   #         [@board[0][1],@board[1][1],@board[2][1],@board[3][1],@board[4][1],@board[5][1]],
-   #         [@board[0][2],@board[1][2],@board[2][2],@board[3][2],@board[4][2],@board[5][2]],
-   #         [@board[0][3],@board[1][3],@board[2][3],@board[3][3],@board[4][3],@board[5][3]],
-   #         [@board[0][4],@board[1][4],@board[2][4],@board[3][4],@board[4][4],@board[5][4]],
-   #         [@board[0][5],@board[1][5],@board[2][5],@board[3][5],@board[4][5],@board[5][5]]]
-   # end
    def add_piece(column,symbol)
        @column=column
        if column_avaliable?
-       		puts "hellothere"
-       		i=6
-       		while true
-       			if @board[column.to_i][i] == nil
-       				@board[column.to_i][i]= symbol
-       				 @rowcoordinate=i
-       				break
-       			else
-       				i-=1
-       				next
-       			end
-       		end
-       		return true
+          i=6
+          while true
+            if @board[column.to_i][i] == "z"
+              @board[column.to_i][i]= symbol
+               @rowcoordinate=i
+              break
+            else
+              i-=1
+              next
+            end
+          end
+          return true
        else
            false
        end
    end
 
    def column_avaliable?
-       if @board[@column.to_i].size <=7 
-           return true 
-       else 
-           return false 
+       if @board[@column.to_i].size <=6
+           return true
+       else
+           return false
        end
    end
    def winning_combination?
-       winning_vertical? || winning_horizontal? ||winning_diagonal?
-       #winning_diagonal? || winning_vertical? || winning horizontal?
+       winning_vertical? || winning_horizontal? || winning_diagonal?
    end
    def winning_vertical?
-       # coordinate=[@column,@rowcoordinate]
-       # verticalarray=[@board[@column.to_i][@rowcoordinate.to_i],@board[@column.to_i][@rowcoordinate.to_i-1],@board[@column.to_i][@rowcoordinate.to_i-2],@board[@column.to_i][@rowcoordinate.to_i-3]]
-       verticalarray=[@board[@column.to_i][6],@board[@column.to_i][5],
-       @board[@column.to_i][4],@board[@column.to_i][3],@board[@column.to_i][2],
-       @board[@column.to_i][1],@board[@column.to_i][0]]
+       verticalarray=[@board[@column.to_i][5],@board[@column.to_i][4],@board[@column.to_i][3],
+       @board[@column.to_i][2],@board[@column.to_i][1],@board[@column.to_i][0]]
        four_in_a_row?(verticalarray)
    end
    def four_in_a_row?(arr)
        arr.join.to_s.include?("XXXX") || arr.join.to_s.include?("OOOO")
    end
    def winning_horizontal?
-   		horizontalarray=[@board[0][@rowcoordinate],@board[1][@rowcoordinate],@board[2][@rowcoordinate],@board[3][@rowcoordinate],@board[4][@rowcoordinate],@board[5][@rowcoordinate]]
-   		four_in_a_row?(horizontalarray)
+      horizontalarray=[@board[0][@rowcoordinate],@board[1][@rowcoordinate],
+      @board[2][@rowcoordinate],@board[3][@rowcoordinate],@board[4][@rowcoordinate],
+      @board[5][@rowcoordinate], @board[6][@rowcoordinate]]
+      four_in_a_row?(horizontalarray)
    end
 
    def winning_diagonal?
-   		#red
-   		d1= [@board[2][0],@board[3][1],@board[4][2],@board[5][3]]
-   		d2= [@board[1][0],@board[2][1],@board[3][2],@board[4][3],@board[5][4]]
-   		d3= [@board[0][0],@board[1][1],@board[2][2],@board[3][3],@board[4][4],@board[5][5]]	
-		  d4= [@board[0][1],@board[1][2],@board[2][3],@board[3][4],@board[4][5],@board[5][6]]
-   		d5= [@board[0][2],@board[1][3],@board[2][4],@board[3][5],@board[4][6]]
-   		d6= [@board[0][3],@board[1][4],@board[2][5],@board[3][6]]
-   		#green
-   		d7= [@board[3][0],@board[2][1],@board[1][2],@board[0][3]] 
-   		d8= [@board[4][0],@board[3][1],@board[2][2],@board[1][3],@board[0][4]]
-   		d9= [@board[5][0],@board[4][1],@board[3][2],@board[2][3],@board[1][4], @board[0][5]] 
-      d10= [@board[5][1], @board[4][2], @board[3][3], @board[2][4], @board[1][5], @board[0][6]]
-   		d11= [@board[5][2], @board[4][3], @board[3][4], @board[2][5], @board[1][6]]
-      d12= [@board[5][3], @board[4][4], @board[3][5], @board[2][6]]
-      arraydiagonal=[d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12]
+    right_to_left_diag?(@column.to_i,@rowcoordinate.to_i) || left_to_right_diag?(@column.to_i,@rowcoordinate.to_i)
 
-      arraydiagonal.each do |currentdiagonal|
-        return true if four_in_a_row?(currentdiagonal)        
-         end
-         return false
-  	end
+   end
+
+  def right_to_left_diag? (altcol, altrow)
+   n=0
+   i=0
+   checkarr = []
+
+   checkpiececolumn = altcol
+   checkpiecerow = altrow
+
+   4.times do |i|
+     if altcol == -1 || altrow == -1 || altcol == 7 || altrow == 6
+       break
+     else
+       checkarr.push @board[altcol][altrow]
+       altcol -= 1 
+       altrow += 1
+       i += 1
+     end
+   end
+
+
+   altcol = checkpiececolumn-1
+   altrow = checkpiecerow-1
+
+   3.times do |n|
+     if altcol == -1 || altrow == -1 || altcol == 7 || altrow == 6
+       break
+     else 
+      checkarr.unshift @board[altcol][altrow]
+      altcol += 1 
+      altrow -= 1
+      n += 1
+     end 
+   end 
+
+   if four_in_a_row?(checkarr)
+      return true
+    else 
+      return false
+    end 
+
+    end
+
+  def left_to_right_diag? (altcol, altrow)
+    n = 0
+    i = 0
+
+    checkarr = []
+
+    checkpiececolumn = altcol
+    checkpiecerow = altrow
+
+    4.times do |i|
+      if altcol == -1 || altrow == -1 || altcol ==7 || altrow == 6
+        break
+      else
+        checkarr.push @board[altcol][altrow]
+        altcol += 1
+        altrow += 1
+        i += 1
+      end
+    end
+    
+    3.times do |n|
+      if altcol == -1 || altrow == -1 || altcol == 7 || altrow ==6
+        break  
+      else
+      checkarr.unshift @board[altcol][altrow]
+      altcol -= 1
+      altrow -= 1
+      n += 1      
+      end
+    end 
+
+    if four_in_a_row?(checkarr)
+    	return true
+    else
+    	return false
+    end
 end
-
-
-
-
-
+end
